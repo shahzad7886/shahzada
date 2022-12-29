8f1de2f507950874cad29735118539f812d02b71
shahzad7886 <asimshah6763132@gmail.com>
2022-12-28 18:44:33 -0800

    my first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h1>hello from Asi shahzad</h1>
+    </div>
+  );
+}
+
+export default App;
