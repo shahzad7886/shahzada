a4f6b94a9160d0f7fff7d06c5389f66ece74fce6
Asim Shahzad <113654383+shahzad7886@users.noreply.github.com>
2023-10-23 20:26:29 +0500

    push
    dashboard is working but in center.

--- src/component/appbar.js
+++ src/component/appbar.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
 import { AppBar, Toolbar,Link, Button,IconButton, Typography,FormControlLabel,Checkbox,FormGroup } from '@mui/material';
 import PaletteIcon from '@mui/icons-material/Palette';
+import MenuIcon from '@mui/icons-material/Menu';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import Brightness2OutlinedIcon from '@mui/icons-material/Brightness2Outlined';
+import makeStyles from "@mui/styles/makeStyles";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(5),
+  },
+}));
+
+
+
 const Navbar = ({ toggleSidebar,darkMode,toggleDarkMode }) => {
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
+     
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Your E-Commerce Website
--- src/seller/sellerDashboard.js
+++ src/seller/sellerDashboard.js
@@ -1,181 +1,154 @@
-// import React from 'react';
-// import {
-//   AppBar,
-//   Toolbar,
-//   Typography,
-//   Drawer,
-//   List,
-//   ListItem,
-//   ListItemText,
-//   Container,
-//   CssBaseline,
-// } from '@mui/material';
-// 
-// import { Link, Route, Routes } from 'react-router-dom';
-
-// import DashboardComponent from '../pages/pages/dashboard';
-// import ProductsComponent from '../pages/pages/product';
-// import OrdersComponent from '../pages/pages/Orders';
+// import React, { useState } from 'react';
+// import { AppBar,
+//     Toolbar,
+//     Typography,
+//     Drawer,
+//     List,
+//     MenuItem,
+//     MenuList,
+//     Menu,
+//     Button,
+//     ListItem,
+//     ListItemText,
+//     IconButton,
+//     ListItemIcon,
+//     Container,
+//     CssBaseline,} from '@mui/material';
+// import MenuIcon from '@mui/icons-material/Menu'
+// import makeStyles from "@mui/styles/makeStyles";
+// import { Link, Route, Switch, useHistory } from 'react-router-dom';
 
 // const drawerWidth = 240;
 
 // const useStyles = makeStyles((theme) => ({
 //   // ... your existing styles
+//   logoutButton: {
+//     marginLeft: 'auto',
+//   },
 // }));
 
 // function SellerDashboard() {
 //   const classes = useStyles();
+//   // const history = useHistory();
+//   const [isLoggedIn, setIsLoggedIn] = useState(
+//     localStorage.getItem('isLoggedIn') === 'true'
+//   );
 
+//   const handleLogout = () => {
+//     // Clear user authentication status and token (if applicable)
+//     localStorage.removeItem('isLoggedIn');
+//     setIsLoggedIn(false);
+//     // Redirect to the login page or any other desired destination
+//     // history.push('/login');
+//   };
+
+//   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+//      const toggleDrawer = () => {
+//        setIsDrawerOpen(!isDrawerOpen);
+//      };
 //   return (
 //     <div className={classes.root}>
 //       <CssBaseline />
 //       <AppBar position="fixed" className={classes.appBar}>
 //         <Toolbar>
+
+//         <IconButton
+//         color="inherit"
+//         aria-label="open drawer"
+//         edge="start"
+//         onClick={toggleDrawer}
+//         className={classes.menuButton}
+//       >
+//         <MenuIcon />
+//       </IconButton>
 //           <Typography variant="h6">Seller Dashboard</Typography>
+//           {isLoggedIn && (
+//             <Button
+//               className={classes.logoutButton}
+//               color="inherit"
+//               onClick={handleLogout}
+//             >
+//               Logout
+//             </Button>
+//           )}
 //         </Toolbar>
 //       </AppBar>
 //       <Drawer
+//         open={isDrawerOpen}
+//         onClose={toggleDrawer}
 //         className={classes.drawer}
-//         variant="permanent"
 //         classes={{
 //           paper: classes.drawerPaper,
 //         }}
-//         anchor="left"
 //       >
-//         <List>
-//           <ListItem button component={Link} to="/dashboard">
-//             <ListItemText primary="Dashboard" />
-//           </ListItem>
-//           <ListItem button component={Link} to="/products">
-//             <ListItemText primary="Products" />
-//           </ListItem>
-//           <ListItem button component={Link} to="/orders">
-//             <ListItemText primary="Orders" />
-//           </ListItem>
-//           {/* Add more menu items as needed */}
-//         </List>
+//         <div>
+//           <List>
+//             <ListItem button>
+//               <ListItemText primary="Menu Item 1" />
+//             </ListItem>
+//             <ListItem button>
+//               <ListItemText primary="Menu Item 2" />
+//             </ListItem>
+//             <ListItem button>
+//               <ListItemText primary="Menu Item 3" />
+//             </ListItem>
+//           </List>
+//         </div>
 //       </Drawer>
-//       <main className={classes.content}>
-//         <Container>
-//           <Routes>
-//             <Route path="/dashboard">
-//               {/* Dashboard component */}
-//               <DashboardComponent />
-//             </Route>
-//             <Route path="/products">
-//               {/* Products component */}
-//               <ProductsComponent />
-//             </Route>
-//             <Route path="/orders">
-//               {/* Orders component */}
-//               <OrdersComponent />
-//             </Route>
-//           </Routes>
-//         </Container>
-//       </main>
+//       {/* ... rest of your code */}
 //     </div>
 //   );
 // }
 
 // export default SellerDashboard;
-import React,{useState} from 'react';
-// import  useNavigate  from 'react-router-dom';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Drawer,
-  List,
-  Button,
-  ListItem,
-  ListItemText,
-  Container,
-  CssBaseline,
-} from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-const drawerWidth = 240;
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
-
-function SellerDashboard({ isOpen, toggleSidebar,sidebarRef}) {
-    const classes = useStyles();
-    // const history = useNavigate();
-    const [isLoggedIn, setIsLoggedIn] = useState(
-      localStorage.getItem('isLoggedIn') === 'true'
-    );
-    
-    const handleLogout = () => {
-      // Clear user authentication status and token (if applicable)
-      localStorage.removeItem('isLoggedIn');
-      setIsLoggedIn(false);
-      // Redirect to the login page or any other desired destination
-    //   history.push('/login');
-    };
+function SellerDashboard() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-       <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" sx={{flexGrow:1}}>Seller Dashboard</Typography>
-          {isLoggedIn && (
-            <Button
-              className={classes.logoutButton}
-              color="inherit"
-              onClick={handleLogout}
-            >
-              Logout
-            </Button>
-          )}
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        open={isOpen} ref={sidebarRef}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <List>
-          <ListItem button>
-            <ListItemText primary="Dashboard" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="Products" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="Orders" />
-          </ListItem>
-          {/* Add more menu items as needed */}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <Container>
-          {/* Seller dashboard content goes here */}
-        </Container>
-      </main>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
